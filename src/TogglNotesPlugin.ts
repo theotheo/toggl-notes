9b132f77f00f5ed06b020554594a7bfe8c453495
theotheo <ibelyalov@yandex.ru>
2024-01-20 21:16:14 +0300

    refactor: encapsulate toggl and frontmatter code

--- src/TogglNotesPlugin.ts
+++ src/TogglNotesPlugin.ts
@@ -1,6 +1,16 @@
 import moment from "moment";
-import { Plugin, Notice, App, PluginSettingTab, Setting } from "obsidian";
-import { Toggl } from 'toggl-track';
+import {
+  Plugin,
+  Notice,
+  App,
+  PluginSettingTab,
+  Setting,
+  TFile
+} from "obsidian";
+import {
+  ITimeEntry,
+  Toggl
+} from "toggl-track";
 
 interface TogglNotesSettings {
   apiToken: string;
@@ -10,73 +20,110 @@ const DEFAULT_SETTINGS: Partial<TogglNotesSettings> = {
   // apiToken: "",
 };
 
+class TogglManager {
+  private client: Toggl;
+
+  constructor(token: string) {
+    this.client = new Toggl({
+      auth: {
+        token: token
+      },
+    });
+
+  }
+
+  public async startTimer(description: string): Promise<ITimeEntry> {
+    const togglParams = await this.client.me.get();
+    const defaultWorkspaceId = togglParams["default_workspace_id"];
+
+    const time_entry = await this.client.timeEntry.create(defaultWorkspaceId, {
+      "description": description,
+      "created_with": "Toggl Notes",
+      "start": moment().format(),
+      "workspace_id": defaultWorkspaceId,
+      "duration": -1
+    });
+
+    return time_entry;
+  }
+
+  public async stopTimer(): Promise<boolean> {
+    const currentEntry = await this.client.timeEntry.current();
+
+    if (currentEntry) { // if timer is running
+      await this.client.timeEntry.stop(currentEntry.id, currentEntry.workspace_id);
+
+      return true;
+    }
+
+    return false;
+  }
+
+}
+
+class FrontMatterManager {
+  private field = "_time_entries";
+  private app: App;
+
+  constructor(app: App) {
+    this.app = app;
+  }
+
+  public async add(file: TFile, id: number) {
+    const fm = this.app.metadataCache.getCache(file.path)?.frontmatter || {};
+    const time_entries: string[] = fm[this.field];
+    time_entries.push(String(id)); // frontmatter don't good with number
+    await this.app.fileManager.processFrontMatter(file, (fm) => fm[this.field] = time_entries);
+  }
+}
+
 export default class TogglNotesPlugin extends Plugin {
   settings!: TogglNotesSettings;
+  togglManager!: TogglManager;
+  frontmatter!: FrontMatterManager;
 
-
-  public override async onload(): Promise<void>  {
+  public override async onload(): Promise<void> {
     this.app.workspace.onLayoutReady(this.onLayoutReady.bind(this));
 
     await this.loadSettings();
 
     this.addSettingTab(new TogglNotesTab(this.app, this));
 
-    const toggl = new Toggl({
-      auth: {
-        token: this.settings.apiToken
-      },
-    })
-
-    const togglParams = await toggl.me.get()
-    const defaultWorkspaceId = togglParams['default_workspace_id']
+    this.togglManager = new TogglManager(this.settings.apiToken);
+    this.frontmatter = new FrontMatterManager(this.app);
 
 
     this.addCommand({
       id: "start-toggl",
       name: "Start new toggl timer",
       callback: async () => {
-    
-        const file = this.app.workspace.getActiveFile()
+        const file = this.app.workspace.getActiveFile();
+
         if (file) {
-          const path = file.path
-          const time_entry = await toggl.timeEntry.create(defaultWorkspaceId, {
-            'description': path,
-            'created_with': "Obsidian client",
-            'start': moment().format(),
-            'workspace_id': defaultWorkspaceId,
-            'duration': -1
-          })
-      
-          const fm = this.app.metadataCache.getCache(file.path)?.frontmatter || {}      
-          let time_entries: string[] = fm['_time_entries']
+          const time_entry = await this.togglManager.startTimer(file.path);
+          this.frontmatter.add(file, time_entry.id);
 
-          time_entries.push(String(time_entry.id)) // frontmatter don't good with number
-
-          await this.app.fileManager.processFrontMatter(file, (fm) => fm['_time_entries'] = time_entries);
-      
           const item = this.addStatusBarItem();
+          item.empty()
           item.createEl("span", { text: "🟢 toggl is running" });
-          new Notice('▶ time entries was started')
-        } 
-
-      },
+          new Notice("▶ time entries was started");
+        }
+      }
     });
 
     this.addCommand({
       id: "stop-toggl",
       name: "Stop current toggl timer",
       callback: async () => {
-        const currentEntry = await toggl.timeEntry.current()
-
-        if (currentEntry) { // if timer is running
-          await toggl.timeEntry.stop(currentEntry.id, currentEntry.workspace_id)
-
+        if (await this.togglManager.stopTimer()) { // if timer was running
           const item = this.addStatusBarItem();
+          item.empty()
           item.createEl("span", { text: "⚪ toggl is stopped" });
-          new Notice('▶ time entries was stopped')
+
+          new Notice("▶ time entries was stopped");
         }
       }
-    })
+    });
   }
 
   async loadSettings() {
@@ -101,7 +148,7 @@ export class TogglNotesTab extends PluginSettingTab {
   }
 
   display(): void {
-    let { containerEl } = this;
+    const { containerEl } = this;
 
     containerEl.empty();
 
